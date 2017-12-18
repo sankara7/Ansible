46916d1c620c1c95cd164d6a021aaaa29332eee8
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-12-18 20:49:23 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -7,7 +7,7 @@ apt-get update
 apt-get install ansible -y
 apt-get install unzip -y
 
-wget https://github.com/sankara7/Ansible/blob/master/Ansible1.zip?raw=true
+wget https://github.com/sankara7/Ansible/raw/master/Configfiles.zip
 unzip Configfiles.zip -d /home
 
 cd /home
