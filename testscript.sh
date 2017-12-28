bf0f975271ee1d6292c98c79217ea36bf901b482
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-12-28 18:42:27 +0530

    Add files via upload

---
+++ testscript.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script parameters from arguments
+Java= $1
+Docker= $2
+Hostmetrics = $3
+echo "$Java" >> textfile.txt
+echo "$Docker" >> textfile.txt
+echo "$Hostmetrics" >> textfile.txt
+
+apt-get update
+apt-get install software-properties-common -y
+apt-add-repository ppa:ansible/ansible -y
+apt-get update
+apt-get install ansible -y
+apt-get install unzip -y
+
+wget https://github.com/sankara7/Ansible/raw/master/Configfiles.zip
+unzip Configfiles.zip -d /home
