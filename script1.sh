5592b68041cd615d9944ca2426be09e1e3cb6afd
sankara7 <30866209+sankara7@users.noreply.github.com>
2017-12-29 18:19:20 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -4,6 +4,7 @@
 Java=$1
 Docker=$2
 Hostmetrics=$3
+JavaIP=$4
 var=$(dig +short myip.opendns.com @resolver1.opendns.com)
 echo $var >> /home/textfile.txt
 echo "$Java" >> /home/textfile.txt
@@ -20,4 +21,11 @@ apt-get install unzip -y
 wget https://github.com/sankara7/Ansible/raw/master/Configfiles.zip
 unzip Configfiles.zip -d /home
 
+if [ "$Java" = "yes" ]
+then
+  echo "condition check pass" >> /home/textfile.txt
+fi
+
+sed -i -e 's/52.172.40.33/$var/g' /home/Configfiles/telegraf.conf
+sed -i -e 's/18.217.53.35/$JavaIP/g' /home/Configfiles/telegraf.conf
 
